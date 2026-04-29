167fd5ffae0cb7e91201b8398d538bdb48dfde58
SlothInTheHat <lalwaninikhil42@gmail.com>
2026-04-28 23:53:12 -0500

    fix: redirect root to /workshops dashboard

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
-  // Don't load dummy workshop data on homepage
-  return { workshop: null, teams: [], participants: [], useCaseCount: 0 };
+export const load: PageServerLoad = async () => {
+  redirect(303, '/workshops');
 };
